677cac0300919b90b9a7f8dfbcfc99ac8ff9619f
Talha Çankaya <67420710+talhacankaya@users.noreply.github.com>
2023-02-18 21:06:33 +0300

    Create homework8.sql

---
+++ homework8.sql
@@ -0,0 +1,3 @@
+test veritabanınızda employee isimli sütun bilgileri id(INTEGER), name VARCHAR(50), birthday DATE, email VARCHAR(100) olan bir tablo oluşturalım.
+
+SELECT * FROM employee (id INTEGER PRIMARY KEY, name VARCHAR (50) NOT NULL, birthday DATE NOT NULL, email VARCHAR (100)
